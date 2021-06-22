b0d443cd8b242ddbaa495807ba194e0bcf6a89c7
Sergei Putkin <s.putkin@iilab.ru>
2021-06-22 18:48:32 +0300

    feat: remake MainActivity.kt with multiple fragments

--- app/src/main/java/com/example/bestvideoloader/MainActivity.kt
+++ app/src/main/java/com/example/bestvideoloader/MainActivity.kt
@@ -1,90 +1,30 @@
 package com.example.bestvideoloader
 
+import android.content.Context
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
-import android.widget.Button
-import android.widget.ProgressBar
-import android.widget.Toast
-import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
-import com.example.bestvideoloader.models.VKVideoFileUploadInfo
-import com.example.bestvideoloader.requests.VKVideoPostCommand
-import com.vk.api.sdk.*
-import com.vk.api.sdk.auth.VKAccessToken
-import com.vk.api.sdk.auth.VKAuthCallback
-import com.vk.api.sdk.auth.VKScope
-import okhttp3.*
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var progress: ProgressBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        VK.login(this, arrayListOf(VKScope.VIDEO))
+        startChildToParent()
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        val callback = object : VKAuthCallback {
-            override fun onLogin(token: VKAccessToken) {
-                initView()
-            }
+    private fun startChildToParent() {
+        supportFragmentManager.beginTransaction()
+            .add(R.id.container, ListVideosFragment::class.java, null)
+            .commit()
+    }
 
-            override fun onLoginFailed(errorCode: Int) {
-            }
+    companion object {
+        fun startFrom(context: Context) {
+            val intent = Intent(context, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+            context.startActivity(intent)
         }
-
-        if (data == null || !VK.onActivityResult(requestCode, resultCode, data, callback))
-            super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == 1) {
-            val contentURI: Uri? = data?.data
-            val videoView = findViewById<VideoView>(R.id.vv)
-            videoView.setVideoURI(contentURI)
-            videoView.requestFocus()
-            videoView.start()
-
-            if (contentURI != null) {
-                sendMyVideo(contentURI)
-            }
-        }
-    }
-
-    private fun initView() {
-        val btn = findViewById<Button>(R.id.btn)
-        btn.setOnClickListener {
-            chooseVideoFromGallery()
-        }
-        progress = findViewById(R.id.simple_progress)
-    }
-
-    private fun chooseVideoFromGallery() {
-        val galleryIntent = Intent(
-            Intent.ACTION_PICK,
-            MediaStore.Video.Media.EXTERNAL_CONTENT_URI
-        )
-        startActivityForResult(galleryIntent, 1)
-    }
-
-    private fun sendMyVideo(uri: Uri) {
-        VK.execute(
-            VKVideoPostCommand(uri, ::setUpProgress),
-            object : VKApiCallback<VKVideoFileUploadInfo> {
-                override fun fail(error: Exception) {
-                    Toast.makeText(this@MainActivity, "fail", Toast.LENGTH_SHORT).show()
-                }
-
-                override fun success(result: VKVideoFileUploadInfo) {
-                    Toast.makeText(this@MainActivity, "success", Toast.LENGTH_SHORT).show()
-                }
-            })
-    }
-
-    private fun setUpProgress(value: Int, maxValue: Int) {
-        progress.max = maxValue
-        progress.progress = value
     }
 }
-
